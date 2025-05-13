26976cc079e5edf1bdb6585ac4bdb9e8e963e18d
trirgg <triramdhani00@gmail.com>
2025-05-13 18:11:01 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <p className="text-red-700 text-6xl ">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
